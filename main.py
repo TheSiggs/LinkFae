a687c065ebb1997b18e5a59dff36d38e02e58571
TheSiggs <samsiggs00@gmail.com>
2023-11-17 23:08:01 +1300

    Added global authentication

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import os
 import validators
 from fastapi import FastAPI, HTTPException, Depends, Request
+from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 from starlette.responses import RedirectResponse
 from src.Database.Database import engine, Base, SessionLocal
 from src.Repository import URLRepository
 from src.Repository.URLRepository import get_url_by_key
 from src.Schema.URL.URLBase import URLBase
+from typing import Annotated
 
 Base.metadata.create_all(bind=engine)
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='')
 
 
 def get_db():
@@ -31,7 +35,9 @@ def raise_bad_request(message):
 
 
 @app.post("/url")
-def create_url(url: URLBase, db: Session = Depends(get_db)):
+def create_url(url: URLBase, token: Annotated[str, Depends(oauth2_scheme)], db: Session = Depends(get_db)):
+    if token != os.getenv('SECRET_KEY'):
+        raise HTTPException(status_code=403, detail="Forbidden")
     if not validators.url(url.target_url):
         raise_bad_request(message="Your provided URL is not valid")
     url = URLRepository.create_url(db=db, url=url.target_url)
